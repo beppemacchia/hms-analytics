40b4bc24a825a452552a7df30ec95043829b3a81
Giuseppe <beppemacchia@outlook.it>
2022-08-13 18:12:16 +0200

    Upgrade Capacitor 4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export * from './definitions';
+import { registerPlugin } from "@capacitor/core";
+
+import type { HmsAnalyticsPlugin } from "./definitions";
+
+const HmsAnalytics = registerPlugin<HmsAnalyticsPlugin>(
+  "HmsAnalytics",
+  {
+    web: () => null,
+  }
+);
+
+export * from "./definitions";
+export { HmsAnalytics };
